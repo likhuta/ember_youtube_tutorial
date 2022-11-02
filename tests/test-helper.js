e53a6b6bf7c654ea4e35b9e7ed3cd5b44908d21c
“Hanna <annalikhuta@gmail.com>
2022-11-02 16:38:52 +0100

    init project, add sass

---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'youtube-tutorial/app';
+import config from 'youtube-tutorial/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
