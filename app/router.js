449771672373c1de1b146d231f2e61673b9897ba
“Hanna <annalikhuta@gmail.com>
2022-11-03 14:21:53 +0100

    init cart and add cart controller

--- app/router.js
+++ app/router.js
@@ -6,4 +6,8 @@ export default class Router extends EmberRouter {
   rootURL = config.rootURL;
 }
 
-Router.map(function () {});
+Router.map(function () {
+  this.route('item', { path: '/item/:item_id' });
+  this.route('not-found', { path: '/*path' });
+  this.route('cart', { path: 'shopping-cart' });
+});
