51985f08be02fd4f1feea7c16da71e865586cbb0
“Hanna <annalikhuta@gmail.com>
2022-11-04 09:58:21 +0100

    use data for product info, display product list

--- app/routes/item.js
+++ app/routes/item.js
@@ -1,8 +1,12 @@
 import Route from '@ember/routing/route';
+import { products } from '../data/products';
 
 export default class ItemRoute extends Route {
   model(params) {
     const { item_id } = params;
-    return item_id;
+    const product = products.find(({ id }) => {
+      return id === item_id;
+    });
+    return product;
   }
 }
